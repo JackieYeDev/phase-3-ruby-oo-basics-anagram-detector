ea2e13fcfc4645d9061a10be4878e7d7046106c5
Jackie <jye0325@gmail.com>
2022-07-25 05:11:53 -0400

    commit tree 8833bcc1a34e3679941b6d4ff62f067e7f7fcfd3

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+    def initialize (word)
+        @word = word
+    end
+
+    def match(list_of_words)    
+        list_of_words.filter {|regex| regex.chars.sort === @word.chars.sort}
+    end
+end
